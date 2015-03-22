c4930af84a3ea995ed36227513c96f0ac3dbef24
Ramzi Chennafi <razc411@hotmail.ca>
2015-03-21 19:55:11 -0700

    Moving

--- src/connection_controller.c
+++ src/connection_controller.c
@@ -5,18 +5,19 @@
     unsigned char tcp_buffer[IP_MAXPACKET];
     int total_packets = 0;
 
-    int monitor_sockets(int udp_watch_sockets, int tcp_watch_sockets)
+    int monitor_sockets(int udp_watch_socket, int tcp_watch_socket)
     {
         int udp_data_size= 0, tcp_data_size = 0;
         struct sockaddr udp_saddr, tcp_saddr;
-        int saddr_size = sizeof(udp_saddr);
+        int saddr_size = sizeof(tcp_saddr);
         while(true)
         {
-            tcp_data_size = recvfrom(tcp_watch_sockets, &tcp_buffer, IP_MAXPACKET, &tcp_saddr, &saddr_size);
+            memset(tcp_saddr, 0, saddr_size);
+            tcp_data_size = recvfrom(tcp_watch_socket, &tcp_buffer, IP_MAXPACKET, &tcp_saddr, &saddr_size);
             check_tcp_packet(&tcp_buffer, tcp_data_size, &tcp_saddr);
             
-            udp_data_size = recvfrom(udp_watch_sockets, &udp_buffer, IP_MAXPACKET, &udp_saddr, &saddr_size);
-            check_udp_packet(&udp_buffer, udp_data_size, &udp_saddr);
+            //udp_data_size = recvfrom(udp_watch_sockets, &udp_buffer, IP_MAXPACKET, &udp_saddr, &saddr_size);
+            //check_udp_packet(&udp_buffer, udp_data_size, &udp_saddr);
         }
     }
 
@@ -39,10 +40,23 @@
     }
 
 
-    int check_udp_packet(unsigned char * buffer, int size, struct sockaddr * udp_saddr)
-    {
+    // int check_udp_packet(unsigned char * buffer, int size, struct sockaddr * udp_saddr)
+    // {
+    //     struct ip * ip_head = (struct iphdr *) buffer;   
+    //     int iphdr_len = ip_head->ip_hl * 4;   
 
-    }
+    //     struct udphdr * udp_head = (struct udpheader *) buffer + iphdr_len;
+
+    //     if(check_port(udp_head->th_sport, udp_head->th_dport))
+    //     {
+    //         modify_port(buffer, UDP);
+    //     } 
+
+    //     if(check_ip(ip_head->ip_src, ip_head->ip_dst))
+    //     {
+    //         modify_ip(buffer, UDP);
+    //     }
+    // }
 
     int check_port(int sport, int dport, struct in_addr src_ip, struct in_addr dest_ip)
     {
